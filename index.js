ca9e45520f042581d3c5cc0d30c1960a6971f05b
Daniel Aston <daniel.aston@live.co.uk>
2024-06-12 10:59:20 +0100

    add mvc structure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import savedRoutes from "./routes/savedRoutes.js"
 
 const PORT = process.env.PORT;
 const BACKEND_URL = process.env.BACKEND_URL;
@@ -9,6 +10,8 @@ const FRONTEND_URL = process.env.FRONTEND_URL;
 const app = express();
 
 app.use(cors({ origin: FRONTEND_URL }));
+
+app.use("/saved", savedRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is started on: ${BACKEND_URL}:${PORT}`);
